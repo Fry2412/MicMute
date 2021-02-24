1b4c4df2c86a0ef491d4ec3da4fe205adcdd949a
Dominik Metz <dominik.metz@gmail.com>
2021-02-24 12:58:03 +0100

    basic chroma support

--- MainForm.cs
+++ MainForm.cs
@@ -6,7 +6,9 @@ using System;
 using System.Drawing;
 using System.Reactive;
 using System.Windows.Forms;
-
+using Colore;
+using Colore.Effects.Keyboard;
+using Colore.Effects.Headset;
 
 namespace MicMute
 {
@@ -18,8 +20,9 @@ namespace MicMute
         private readonly string registryKeyName = "Hotkey";
 
         private Hotkey hotkey;
-
-        private bool myVisible; 
+        private IChroma chroma;
+        private KeyboardCustom muted;
+        private bool myVisible;
         public bool MyVisible
         {
             get { return myVisible; }
@@ -29,6 +32,26 @@ namespace MicMute
         public MainForm()
         {
             InitializeComponent();
+
+            initChromaSkd();
+
+        }
+
+        private void initChromaSkd()
+        {
+            var t = ColoreProvider.CreateNativeAsync();
+            t.Wait();
+            this.chroma = t.Result;
+
+            this.muted = KeyboardCustom.Create();
+            this.muted.Set(Colore.Data.Color.Red);
+            this.muted[Key.Macro4] = Colore.Data.Color.Green;
+
+            this.unmuted = KeyboardCustom.Create();
+            this.unmuted.Set(Colore.Data.Color.Green);
+            this.unmuted[Key.Macro4] = Colore.Data.Color.Red;
+
+
         }
 
         private void OnNextDevice(DeviceChangedArgs next)
@@ -50,11 +73,19 @@ namespace MicMute
             CenterToScreen();
         }
 
+
         private void MainForm_Load(object sender, EventArgs e)
         {
             MyHide();
             UpdateDevice(AudioController.DefaultCaptureDevice);
             AudioController.AudioDeviceChanged.Subscribe(OnNextDevice);
+
+            var timer = new Timer() { Interval = 500, Enabled = true };
+
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+
 
             var hotkeyValue = registryKey.GetValue(registryKeyName);
             if (hotkeyValue != null)
@@ -66,8 +97,67 @@ namespace MicMute
 
             //AudioController.AudioDeviceChanged.Subscribe(x =>
             //{
-            //    Debug.WriteLine("{0} - {1}", x.Device.Name, x.ChangedType.ToString());
+            //    Debug.WriteLine("{ 0} - {1}", x.Device.Name, x.ChangedType.ToString());
             //});
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!isDeviceInUse())
+            {
+                if (chroma.Initialized)
+                    chroma.UninitializeAsync();
+            }
+            else
+            {
+                if (!chroma.Initialized)
+                {
+                    initChromaSkd();
+                    updateKeyboardStatus(AudioController.DefaultCaptureDevice);
+                }
+                else
+                    updateKeyboardStatus(AudioController.DefaultCaptureDevice);
+            }
+        }
+
+        private static bool isDeviceInUse()
+        {
+            var subkeys = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone");
+            foreach (var sub in subkeys.GetSubKeyNames())
+            {
+                var subsub = subkeys.OpenSubKey(sub);
+                if (subsub.SubKeyCount == 0)
+                {
+                    // LastUsedTimeStart
+                    // LastUsedTimeStop
+                    var lastStart = subsub.GetValue("LastUsedTimeStart");
+                    var lastStop = subsub.GetValue("LastUsedTimeStop");
+                    var startDate = new DateTime((long)lastStart);
+                    var stopDate = new DateTime((long)lastStop);
+                    if (startDate > stopDate)
+                        return true;
+
+                }
+                else
+                {
+                    foreach (var key in subsub.GetSubKeyNames())
+                    {
+                        var np = subsub.OpenSubKey(key);
+                        var lastStart = np.GetValue("LastUsedTimeStart");
+                        var lastStop = np.GetValue("LastUsedTimeStop");
+                        var startDate = new DateTime((long)lastStart);
+                        var stopDate = new DateTime((long)lastStop);
+                        if (startDate > stopDate)
+                        {
+                            //Console.WriteLine(key);
+                            //Console.WriteLine(startDate);
+                            //Console.WriteLine(stopDate);
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         private void OnMuteChanged(DeviceMuteChangedArgs next)
@@ -76,7 +166,7 @@ namespace MicMute
         }
 
         IDisposable muteChangedSubscription;
-        public void UpdateDevice(IDevice device)
+        public void UpdateDevice(AudioSwitcher.AudioApi.IDevice device)
         {
             muteChangedSubscription?.Dispose();
             muteChangedSubscription = device?.MuteChanged.Subscribe(OnMuteChanged);
@@ -86,18 +176,31 @@ namespace MicMute
         Icon iconOff = Properties.Resources.off;
         Icon iconOn = Properties.Resources.on;
         Icon iconError = Properties.Resources.error;
+        private KeyboardCustom unmuted;
+        private bool lastMutedState;
 
-        public void UpdateStatus(IDevice device)
+        public void UpdateStatus(AudioSwitcher.AudioApi.IDevice device)
         {
             if (device != null)
             {
                 UpdateIcon(device.IsMuted ? iconOff : iconOn, device.FullName);
+                updateKeyboardStatus(device);
+
             }
             else
             {
                 UpdateIcon(iconError, "< No device >");
             }
         }
+
+        private void updateKeyboardStatus(AudioSwitcher.AudioApi.IDevice device)
+        {
+            if (chroma.Initialized)
+            {
+                chroma.Keyboard.SetCustomAsync(device.IsMuted ? this.muted : this.unmuted);
+            }
+        }
+
         private void UpdateIcon(Icon icon, string tooltipText)
         {
             this.icon.Icon = icon;
